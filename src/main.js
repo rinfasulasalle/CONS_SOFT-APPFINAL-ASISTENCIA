ffa72dc777adecde227a2a9fd1b04a272709b71c
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-06-20 23:58:33 -0500

    a

--- src/main.js
+++ src/main.js
@@ -17,8 +17,15 @@ import 'primeicons/primeicons.css';
 
 // cada componente se importa de forma separada +++++++++++++++++++++++++
 import Button from "primevue/button"
+
+// Router
 import Menubar from "primevue/menubar"
 import InputText from "primevue/inputtext"
+//datatable
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
 
 
 const app = createApp(App);
@@ -30,6 +37,9 @@ app.component('Button', Button);
 app.component('Menubar', Menubar);
 app.component('InputText', InputText);
 
-
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
 
 app.mount("#app")
