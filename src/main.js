2d7393e54a8dda7b159d7d537fb282e9d82e7f5a
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-06-20 22:33:15 -0500

    a

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import
+import PrimeVue from 'primevue/config';
+//import "primevue/resources/themes/md-dark-indigo/theme.css"; 
+//import "primevue/resources/themes/md-light-indigo/theme.css"; 
+//import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+import "primevue/resources/themes/viva-dark/theme.css";
+//import "primevue/resources/themes/viva-light/theme.css";
+//core
+import "primevue/resources/primevue.min.css";
+//icons
+import 'primeicons/primeicons.css';
+
+// cada componente se importa de forma separada +++++++++++++++++++++++++
+import Button from "primevue/button"
+
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+// aqui agregamos el componente ******************************************
+app.component('Button', Button);
+
+
+
+
+app.mount("#app")
